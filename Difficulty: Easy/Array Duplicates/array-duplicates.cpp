e21ae18038b59aa47549e15b49a547e84a7528d1
Shruti Kumari <157979575+shrutijaiswal2905@users.noreply.github.com>
2025-06-05 12:26:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+        unordered_map<int, int> freq;
+        vector<int> result;
+
+    // Count frequency of each element
+    for (int num : arr) {
+        freq[num]++;
+    }
+
+    // Collect elements that appear more than once
+    for (const auto& pair : freq) {
+        if (pair.second > 1) {
+            result.push_back(pair.first);
+        }
+    }
+
+    return result;
+        
+    }
+};
