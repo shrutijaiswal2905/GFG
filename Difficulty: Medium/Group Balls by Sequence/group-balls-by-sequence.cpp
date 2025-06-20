3e28bfc881daca51623e7de76ce84071300b702b
Shruti Kumari <157979575+shrutijaiswal2905@users.noreply.github.com>
2025-06-20 14:59:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Group Balls by Sequence/group-balls-by-sequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    bool validgroup(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        unordered_map<int, int> mp;
+        for(int i = 0; i < n; i++){
+            mp[arr[i]]++;
+        }
+        for(int i = 0; i < n; i++){
+            int val = arr[i];
+            while(mp[val] > 0){
+                mp[val]--;
+                for(int j = 1; j < k; j++){
+                    if(mp[val + j] == 0){
+                        return false;
+                    }else{
+                        mp[val + j]--;
+                    }
+                }
+            }
+        }
+        return true;
+        
+    }
+};
