6f4e63292f9c002e0deaab4808089daa4b599bc8
Shruti Kumari <157979575+shrutijaiswal2905@users.noreply.github.com>
2025-09-05 15:01:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -1,9 +1,5 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Link list Node */
-class Node {
+/* Node is defined as
+  class Node {
   public:
     int data;
     Node* next;
@@ -13,106 +9,30 @@ class Node {
         next = nullptr;
     }
 };
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-void freeList(struct Node* head) {
-    struct Node* temp;
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        delete temp;
-    }
-}
-
-
-// } Driver Code Ends
-
-/*  Node is defined as
-  struct Node {
-    int data;
-    struct Node *next;
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};*/
+*/
 class Solution {
   public:
     Node* segregate(Node* head) {
-        vector<int> mp(3, 0);
-        Node* start = head;
-        while(start)
-        {
-            mp[start->data]++;
-            start = start->next;
-        }
-        start = head;
-        int num_0 = mp[0], num_1 = mp[1], num_2 = mp[2];
-        
-        for(int i = 0; i < num_0; i++)
-        {
-            start->data = 0;
-            start = start->next;
-        }
-        for(int i = 0; i < num_1; i++)
-        {
-            start->data = 1;
-            start = start->next;
-        }
-        for(int i = 0; i < num_2; i++)
-        {
-            start->data = 2;
-            start = start->next;
-        }
-        return head;
+        // code here
+       if(!head)
+       return nullptr;
+       
+       Node* curr=head;
+       vector<int>ans;
+       while(curr)
+       {
+           ans.push_back(curr->data);
+           curr=curr->next;
+       }
+       sort(ans.begin(),ans.end());
+       curr=head;
+       int i=0;
+       while(curr)
+       {
+           curr->data=ans[i];
+           i++;
+           curr=curr->next;
+       }
+       return head;
     }
 };
-
-
-//{ Driver Code Starts.
-
-// Driver program to test above functions
-int main() {
-    int t, k;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after t
-
-    while (t--) {
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-
-        stringstream ss(input);
-        Node *head = nullptr, *tail = nullptr;
-        int x;
-
-        if (ss >> x) {
-            head = new Node(x);
-            tail = head;
-
-            while (ss >> x) {
-                tail->next = new Node(x);
-                tail = tail->next;
-            }
-
-            // Link the last node to the head if k is 1 to make it circular
-            if (k == 1) {
-                tail->next = head;
-            }
-        }
-
-        Solution ob;
-        Node* newHead = ob.segregate(head);
-        printList(newHead);
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-// } Driver Code Ends
