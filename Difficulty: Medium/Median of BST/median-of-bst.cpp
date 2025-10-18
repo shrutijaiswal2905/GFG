6897f296fcd2f8177684fe36cbe3aa8d8f8e1c8e
Shruti Kumari <157979575+shrutijaiswal2905@users.noreply.github.com>
2025-10-18 13:28:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Median of BST/median-of-bst.cpp
@@ -0,0 +1,33 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+      void inorder(Node*root,vector<int>&ans){
+          if (root==NULL) return;
+          inorder(root->left,ans);
+          ans.push_back(root->data);
+          inorder(root->right,ans);
+      }
+    int findMedian(Node* root) {
+        vector<int>ans;
+        inorder(root,ans);
+        int k=ans.size();
+        int med=0;
+        if(k%2==0){
+            med=ans[(k/2)-1];
+        }  
+        else med= ans[((k+1)/2)-1];
+        return med;
+    }
+};
