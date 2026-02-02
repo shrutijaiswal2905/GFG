ad65705c5b7236f1fae9b570c61fa4bd4d7ab6e6
Shruti Kumari <157979575+shrutijaiswal2905@users.noreply.github.com>
2026-02-02 13:07:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -1,40 +1,33 @@
 class Solution {
   public:
     int maxCircularSum(vector<int> &arr) {
-        // code here
-int n=arr.size();
-        int normal_maxi=INT_MIN;
-        int curr=0;
-        //without circular just normal one
-        for(int i=0;i<n;i++){
-            curr+=arr[i];
-            normal_maxi=max(normal_maxi,curr);
-            if(curr<0){
-                curr=0;
-            }
-        }
-        int mini=INT_MAX;
-        curr=0;
-        int tot=0;
-        bool all_neg=true;
-        //for ciruclar one
-        //circular=tot_sum-min_subarray_sum
-        for(int i=0;i<n;i++){
-            if(arr[i]>=0){
-                all_neg=false;
-            }
-            tot+=arr[i];
-            curr+=arr[i];
-            mini=min(mini,curr);
-            if(curr>0){
-                curr=0;
-            }
-        }
-        int circular=tot-mini;
-        //if all are negative 
-        if(all_neg){
-            return normal_maxi;
-        }
-        return max(normal_maxi,circular);
+           
+           int n = arr.size();
+           
+           int mini = INT_MAX, minsum = 0;
+           int maxi = INT_MIN, maxsum = 0;
+           int total =0;
+           for(int i:arr){
+                    total+=i;
+                    
+                    minsum+=i;
+                    maxsum+=i;
+                    mini = min(mini, minsum);
+                    maxi = max(maxi, maxsum);
+                    if(minsum>0){
+                         minsum=0;
+                    }
+                    if(maxsum<0){
+                          maxsum=0;
+                    }
+                    
+                    
+   
+           }
+           
+        //   cout<<maxi<<" "<<mini<<endl;
+          if(maxi<0) return maxi;
+           
+           return max(maxi, total-mini);
     }
 };
